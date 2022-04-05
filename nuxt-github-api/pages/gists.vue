6603e4b1722afb82dddc1d7cb5384beb71b9c128
Stephan Taylor <stephantaylor21@hotmail.com>
2022-04-05 09:57:52 -0600

    added pages

---
+++ nuxt-github-api/pages/gists.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Gists</h2>
+    <ul>
+      <li v-for="gist in gists" :key="gist.id">
+        {{ gist.description }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+  const { data: gists } = await useAsyncData("gists", () =>
+    $fetch("/api/github")
+  );
+</script>
